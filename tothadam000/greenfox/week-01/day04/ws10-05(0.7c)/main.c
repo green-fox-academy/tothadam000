f937af4a4e8e9c3feab8225efc95c66f5bde36ad
Toth Adam <tothadam000@gmail.com>
2017-10-05 20:58:51 +0200

    debug

--- tothadam000/greenfox/week-01/day04/ws10-05(0.7c)/main.c
+++ tothadam000/greenfox/week-01/day04/ws10-05(0.7c)/main.c
@@ -8,9 +8,14 @@ create a simple program which checks if the num is odd or even
 int main()
 {
     int num;
-    num = 13;
+    num = 32;
 
-
+    switch (num%2) {
+    case 1 : printf("The number is odd.");
+        break;
+    case 0 : printf("The number is even.");
+        break;
+    }
 
     return 0;
 }

add055f96de3f4c28615dd52d1f76d7724c9cfb5
Toth Adam <tothadam000@gmail.com>
2017-10-09 15:49:01 +0200

    end

---
+++ tothadam000/greenfox/week-02/Day01/another_file.h
@@ -0,0 +1,14 @@
+//TODO: solve multiple inclusion problem
+//#include "another_file.c"
+
+
+//Declaration of a function which can be found in another_file.c
+void StringManipulator(char *);
+
+//Declaration of a variable
+int an_example_number;
+
+
+
+//TODO: Declare other things if needed
+void IntegerManipulator(int *);

84a46f71bb177c7b0f8391dd1d35eb6790369f49
Toth Adam <tothadam000@gmail.com>
2017-10-10 17:42:46 +0200

    input4

--- tothadam000/greenfox/week-02/Day02/Input4/main.c
+++ tothadam000/greenfox/week-02/Day02/Input4/main.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-char func();
+
+void func();
 int main()
 {
     //write a void function which asks for a number with scanf and print it out with printf,
     //then asks for a name with gets, and print it out with puts
     //you will notice something is wrong
     //try to solve the problem
-    int typed_number = 0;
 
-    printf("Type a number: \n");
-    scanf("%d", &typed_number);
-    printf("The typed number: %d", typed_number);
-    return 0 ;
 
-    //func();
+    func();
+
+
 
     return 0;
 }
-
-/*char func(typed_number){
-    printf("Type a number: ");
+void func(){
+    int typed_number = 0;
+    char name [25];
+    printf("Type a number: \n");
     scanf("%d", &typed_number);
-    printf("The typed number: %d", typed_number);
-    return 0 ;
+    printf("The typed number: %d\n", typed_number);
+    printf("Give me your name:\n");
+    fflush(stdin);
+    gets(name);
+    puts(name);
+}
 
-}*/

2a6319514e777ef532c0929fdfc4194a4a204f46
Toth Adam <tothadam000@gmail.com>
2017-10-10 15:50:11 +0200

    Input3new

--- tothadam000/greenfox/week-02/Day02/Input3/main.c
+++ tothadam000/greenfox/week-02/Day02/Input3/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+
+char function();
 int main()
 {
     //as you know, a character is represented as a numeric value(ASCII value) in the memory
     //write a function which asks for one charcter and prints out the character in character format then in ASCII too
 
-    char char_to_ascii;
+    function();
+
+    return 0;
+}
+char function(char_to_ascii){
     printf("Type in one character then press enter: ");
     scanf("%c", &char_to_ascii);
-
     printf("ASCII value of %c = %d", char_to_ascii, char_to_ascii);
-    return 0;
+    return ;
 
 }
--- tothadam000/greenfox/week-02/Day02/Input4/main.c
+++ tothadam000/greenfox/week-02/Day02/Input4/main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
+void func(int number);
 int main()
 {
-    //write a void function which asks for a number with scanf and print it out with printf, then asks for a name with gets, and print it out with puts
+    //write a void function which asks for a number with scanf and print it out with printf,
+    //then asks for a name with gets, and print it out with puts
     //you will notice something is wrong
     //try to solve the problem
 
-    func();
+    func(number);
 
     return 0;
+
 }
+
+void func(){
+    int number;
+    scanf("%d", &number);
+    printf("%d\n", number);
+    return 0;
+}
+

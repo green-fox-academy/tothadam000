52592c47ebdfcbca757c3d1035ab36ddbde0e1ab
Toth Adam <tothadam000@gmail.com>
2017-10-12 14:28:53 +0200

    anagram_ifelse

--- tothadam000/greenfox/week-02/Day03/Anagram/main.c
+++ tothadam000/greenfox/week-02/Day03/Anagram/main.c
@@ -1,46 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-
+void not_an_anagram();
 int main(void)
 {
-   /* char word[50];
-    char angram_of_the_word[50];
+    char word[50];
+    char anagram_of_the_word[50];
     printf("Enter a word : ");
     scanf("%s", word);
-    printf("Enter angram of the word : ");
-    scanf("%s", angram_of_the_word);
-
-    for(int i = 0; i < 50; i++){
-        for(int j = 50; j > 0; j--){
-            if(word[i] == angram_of_the_word[j])
-                printf("It's an anagram!");
-            else
-                printf("It's not an anagram!");
+    printf("Enter anagram of the word : ");
+    scanf("%s", anagram_of_the_word);
+    int equal = strcmp(word, anagram_of_the_word);
+        if (equal != 0) {
+            not_an_anagram();
         }
+        else if (equal == 0) {
+            for(int i = 0; i < 50; i++){
+                for(int j = 0; j < 50; j++){
+                    if (){
+                        printf("It's an anagram!");
+                    }else {
+                        printf("It's not an anagram!");
+                    }
+                }
+            }
+        }
+    return 0;
+}
+
+void not_an_anagram(){
+    printf("Words are not anagrams!");
     }
-
-
-   */
-
-
-
-
-
-
-char word[50];
-char end;
-
-printf("Enter a word and I'll give it to you backwards: ");
-
-scanf("%s", word);                                                  //WORD BACKWARD
-
-end = strlen(word) - 1;
-
-    printf("%s",word);
-for (int x = end; x >= 0; --x) {
-    printf("%c", word[x]);
-}
-
-return 0;
-}
 

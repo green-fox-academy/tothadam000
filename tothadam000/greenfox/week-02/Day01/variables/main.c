96354b979fee33961c674fafb90d140b47bc1beb
Toth Adam <tothadam000@gmail.com>
2017-10-13 11:57:33 +0200

    folders

--- tothadam000/greenfox/week-02/Day01/variables/main.c
+++ tothadam000/greenfox/week-02/Day01/variables/main.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
-/*
-Write a program where you have five functions
-first the program should ask for an integer, then
-these functions should do various mathematical operations on a given integer
-last but not least there should be a variable which counts how many operations
-have been done on the given integer
-*/
 int counter = 0;
 int main()
 {
@@ -17,29 +10,23 @@ int main()
     addition();
     subtraction();
     printf("%d", counter);
-
 }
-
 void addition(){
     counter++;
     printf("Add \n");
 }
-
 void subtraction(){
     counter++;
     printf("subtraction \n");
 }
-
 void multiplication(){
     counter++;
     printf("multiplication \n");
 }
-
 void modulo(){
     counter++;
     printf("modulo \n");
 }
-
 void division(){
     counter++;
     printf("Division \n");

c6efb6b82fd5264334f40914ef0f1fa7c39c5df9
Toth Adam <tothadam000@gmail.com>
2017-10-15 21:24:14 +0200

    triangle_not_ended

---
+++ tothadam000/greenfox/week-01/day04(10-05)/triange/triange/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*create a simple program which generates a triangle like this:
+1
+22
+333
+4444
+55555
+666666
+7777777
+if you change the rows variable the output should be different as well
+*/
+int main()
+{
+	int rows;
+	int n;
+    int i;
+
+    printf("Rows:");
+    scanf("%d", &rows);
+
+	for(i = 1; i <= rows; i++)
+	{
+        for(n = 1; n <= i; n++)
+        {
+            printf("%d", rows);
+        }
+        printf("\n");
+	}
+	return 0;
+}

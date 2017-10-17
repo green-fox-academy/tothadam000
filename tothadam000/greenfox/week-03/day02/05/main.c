c99e31fd1993a281a0461c419491a1c3caf35cd0
Toth Adam <tothadam000@gmail.com>
2017-10-17 15:43:38 +0200

    07_added

--- tothadam000/greenfox/week-03/day02/05/main.c
+++ tothadam000/greenfox/week-03/day02/05/main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdint.h>
 //TODO: write a function which returns 485 as unsigned integer value
 //put your return value in an integer variable in which you should have 229, and print it out
 //try to figure out which type casting you should use
 
+uint16_t value();
 
 int main()
 {
+    int a = 229;
+    a = value();
+    printf("%d", a);
 
-    return ;
+    return 0;
 }
-int function ()
-{
-    int a = 500 -- 15;
 
+uint16_t value()
+ {
     return 485;
-}
+ }
--- tothadam000/greenfox/week-03/day02/07/main.c
+++ tothadam000/greenfox/week-03/day02/07/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdint.h>
 int main()
 {
     int i_array[10] = {260, 259, 258, 257, 256, 255, 254, 253, 252, 251};
@@ -8,13 +8,14 @@ int main()
 
 	//TODO: write a program, which copies the values from the given array to an unsigned char array
     //print out the elements of the unsigned char array, with the "%d" format specifier
-    //what do you see?
+    //what do you see? Overflow! We can solve it with uint16_t.
 
 	for(int i = 0; i < 10; ++i) {
     uc_array[i] = i_array[i];
-
-    printf("%d\n", i_array[i]);
+    printf("%d\n", uc_array[i]);
     }
+    printf("\nOverflow! We can solve it with uint16_t.\n");
 
     return 0;
 }
+

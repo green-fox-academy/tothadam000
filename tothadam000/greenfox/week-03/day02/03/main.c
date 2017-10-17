8afde550cc1c241155468897fc223004e30cdb79
Toth Adam <tothadam000@gmail.com>
2017-10-17 12:40:08 +0200

    04_with_functions

--- tothadam000/greenfox/week-03/day02/03/main.c
+++ tothadam000/greenfox/week-03/day02/03/main.c
@@ -2,17 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+int itoa_a();
+int itoa_b();
+int a = 54325;
+char buffer[20];
+
 int main()
 {
-	int a = 54325;
-    char buffer[20];
+	//int a = 54325;
+    //char buffer[20];
 
     //TODO: make an integer to string conversion with itoa(), first in binary, then decimal format
     // Store the string in buffer
-    itoa(a,buffer,2);
-    printf("Binary value = % s \n", buffer);
-    itoa(a,buffer,10);
-    printf("Decimal value = % s \n", buffer);
+    itoa_a();
+    itoa_b();
 
     return 0;
 }
+
+int itoa_a()
+{
+    itoa(a,buffer,2);
+    printf("Binary value = % s \n", buffer);
+}
+
+int itoa_b()
+{
+   itoa(a,buffer,10);
+    printf("Decimal value = % s \n", buffer);
+}
+
+
+
+

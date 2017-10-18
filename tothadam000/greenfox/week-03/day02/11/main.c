d6357dcd35f07ac6b3075a414eadcadc6013eab5
Toth Adam <tothadam000@gmail.com>
2017-10-18 17:24:07 +0200

    011_added

---
+++ tothadam000/greenfox/week-03/day02/11/main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+//TODO: write a register-login-logout program
+    //if you type in register print out "Registration..." or "You are already registered." if you are registered.
+	//you are able to log in, if you are already registered, otherwise not. In that case print out "You are not registered.".
+    //if you type in login print out "Logging in..." or "Already logged in." if are logged in.
+    //if you type in logout print out "Logging out..." or "Already logged out." if are logged out.
+
+int main()
+{
+	char command[256];
+    char logged_in = 0;
+    char registered = 0;
+    char *ret;
+
+    while(1){
+        printf("What the heck?");
+        scanf("%s", command);
+            if (ret = strstr(command)){
+
+            }
+
+
+
+
+}

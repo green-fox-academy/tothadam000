64cfdedda4824b7cfc8a9bc33e668bc03dec4afd
Toth Adam <tothadam000@gmail.com>
2017-10-24 14:22:59 +0200

    10_add

---
+++ tothadam000/greenfox/week-04/day01/10/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//TODO: write a function which swaps the values of x and y. Then print out the values of x and y.
+
+void swap(int *a, int *b);
+
+int main()
+{
+	int x, y;
+	x = 5;
+	y = 8;
+	printf("Before Swapping\nx = %d\ny = %d\n", x, y);
+	swap(&x, &y);
+    printf("After Swapping:\nx = %d\ny = %d\n", x, y);
+
+
+	return 0;
+}
+
+void swap (int *a, int *b)
+{
+    int temp = NULL;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+

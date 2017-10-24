a1eaa9615a1a3267bd2b8f8d79094a70cc604b0b
Toth Adam <tothadam000@gmail.com>
2017-10-24 15:57:37 +0200

    12_add

---
+++ tothadam000/greenfox/week-04/day01/12/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print_array(int *array, int size);
+
+int main()
+{
+	int a[] = {6, 8, 48, 1, -9, 89};
+    print_array(a, (sizeof a / sizeof (int)));
+	//TODO: write a function, which prints out the passed array's elements.
+
+	return 0;
+}
+void print_array(int *array, int size)
+{
+    for (int i = 0; i < size; i++){
+        printf("%d", array[i]);
+    }
+}
+

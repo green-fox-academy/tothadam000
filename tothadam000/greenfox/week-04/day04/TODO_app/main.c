36db7055e09003bbeb177c365a56619322f1c88b
Toth Adam <tothadam000@gmail.com>
2017-10-26 11:37:06 +0200

    TODO_start

---
+++ tothadam000/greenfox/week-04/day04/TODO_app/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void welcome_screen();
+int main()
+{
+welcome_screen();
+
+    return 0;
+}
+void welcome_screen(){
+    printf("\tTodo application\n"
+"==================================\n"
+"Commands:\n"
+" -a   Adds a new task\n"
+" -wr  Write current todos to file\n"
+" -rd  Read todos from a file\n"
+" -l   Lists all the tasks\n"
+" -e   Empty the list\n"
+" -rm   Removes a task\n"
+" -c   Completes a task\n"
+" -p   Add priority to a task\n"
+" -lp  Lists all the tasks by priority\n");
+}

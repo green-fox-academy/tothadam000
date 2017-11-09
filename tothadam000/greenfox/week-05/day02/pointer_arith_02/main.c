f4a05f46aabeb0639e7e82d479b65af7ae7840b1
Toth Adam <tothadam000@gmail.com>
2017-11-09 15:25:21 +0100

    re

--- tothadam000/greenfox/week-05/day02/pointer_arith_02/main.c
+++ tothadam000/greenfox/week-05/day02/pointer_arith_02/main.c
@@ -10,9 +10,6 @@
 int main(){
     int integers[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
     int *apointer = integers;
-  //  printf("address\t\tvalue\n%p\t%d\n",apointer, *apointer);
-    //apointer+=2;
-   // printf("%p\t%d\n",apointer, *apointer);
     int size_array = sizeof (integers) / sizeof (integers[0]);
 
     for(int* ptr = integers; ptr-integers < 16; ptr++){

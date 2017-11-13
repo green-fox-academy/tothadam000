49252c9f412975330d670765a28d31e12aa40b9d
Toth Adam <tothadam000@gmail.com>
2017-11-13 11:44:37 +0100

    x

--- tothadam000/greenfox/week-04/day03/02/main.c
+++ tothadam000/greenfox/week-04/day03/02/main.c
@@ -3,12 +3,12 @@
 // Write a function that takes a Car as an argument and prints all it's stats
 // If the car is a Tesla it should not print it's gas level
 
-typedef enum {VOLVO, TOYOTA, LAND_ROVER, TESLA}CarType;
+typedef enum {VOLVO, TOYOTA, LAND_ROVER, TESLA} CarType;
 
 typedef struct {
-  CarType type;
-  double km;
-  double gas;
+    CarType type;
+    double km;
+    double gas;
 }Car;
 
 int main() {
@@ -29,9 +29,9 @@ int main() {
     return 0;
 }
 void print_car(Car car1){
-    printf( "%.f \n", car1.km);
+    printf( "  %.f\n", car1.km);
     if (car1.type != TESLA){
-        printf("%.f\n", car1.gas);
+        printf("  %.f\n", car1.gas);
     }
 }
 
---
+++ tothadam000/greenfox/week-06/day01/00/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// Modify this program to greet you instead of the World!
+
+int main()
+{
+    string name = "Tibi";
+
+    cout << "Hello "<<name<<"!" <<endl;
+
+    return 0;
+}

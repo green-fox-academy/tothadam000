ed2a12c6853d676900c90d773bdf779c22343f27
Toth Adam <tothadam000@gmail.com>
2017-11-13 16:20:27 +0100

    vector01_add

---
+++ tothadam000/greenfox/week-06/day01/vector01/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	//create an integer vector with the size of 5 and print all elements of it
+    vector <int> int_vector;
+
+    int_vector.push_back(1);
+    int_vector.push_back(2);
+    int_vector.push_back(3);
+    int_vector.push_back(4);
+    int_vector.push_back(5);
+
+    for (unsigned int i = 0; i < int_vector.size(); ++i){
+        cout << int_vector[i] << endl;
+    }
+
+  return 0;
+}
---
+++ tothadam000/greenfox/week-06/day01/vector02/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	//Create a vector of integers
+	//reserve place for 10 element, then fill those place with random numbers between 0 and 10
+    int
+    return 0;
+}

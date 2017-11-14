b70f5914d01ccda7b5c551451097b39eb45a6439
Toth Adam <tothadam000@gmail.com>
2017-11-14 17:51:41 +0100

    oop08_add

---
+++ tothadam000/greenfox/week-06/day02/08/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Class {
+    private:
+
+
+    public:
+        add_grade()
+        get_average()
+int main() {
+    // Create a student Class
+    // that has a method `add_grade`, that takes a grade from 1 to 5
+    // an other method `get_average`, that returns the average of the
+    // grades
+
+  return 0;
+}

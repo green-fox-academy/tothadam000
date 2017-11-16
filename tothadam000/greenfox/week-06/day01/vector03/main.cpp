f59b5891d8c2370f2549745fd6cf74bb54901591
Toth Adam <tothadam000@gmail.com>
2017-11-16 11:34:21 +0100

    vector03_rebuild

--- tothadam000/greenfox/week-06/day01/vector03/main.cpp
+++ tothadam000/greenfox/week-06/day01/vector03/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
+
 using namespace std;
 
 int main() {
@@ -11,15 +12,30 @@ int main() {
   //remove the even numbers, then print the items again
 
     vector <int> int_vector;
-    int size = 20;
+    int_vector.reserve(20);
+    //int size = 20;
+    int i = 0;
 
-    for(unsigned int i = 0; i < size; ++i){
+    for(i = 0; i < int_vector.capacity(); ++i){
         int_vector.push_back(rand() % 10);
-
-        if (int_vector[i] % 2 ==0){
-        cout << int_vector[i] << endl;
-        }
+        cout << int_vector [i] << endl;
     }
+    cout << "--------------------" << endl;
+    for(i = 0; i < int_vector.size(); ){
+        if (int_vector[i] % 2 == 0){
+           int_vector.erase(int_vector.begin()+i);
+        }
+       else{
+            cout << int_vector[i] << endl;
+            i++;
+        }
+
+    }
+
+  /*  for (unsigned int i = 0; i < int_vector.size(); ++i)}
+    cout << ' ' << int_vector[i];
+    cout << '\n';*/
+
 
     return 0;
 }

f23342afdd83c48887f5b0651602c027505bdd63
Toth Adam <tothadam000@gmail.com>
2017-11-17 13:23:38 +0100

    todoadd_engine

--- tothadam000/greenfox/week-06/day04/todo_app/main.cpp
+++ tothadam000/greenfox/week-06/day04/todo_app/main.cpp
@@ -4,72 +4,86 @@
 
 using namespace std;
 
-class todos{
-    public:
+class Todos{
+    protected:
         vector<string> taskVector;
         vector<int> priorityVector;
-        string command = "";
+
         string task = "";
         int priority = 0;
-        //void add_new_task();
-       // void add_priority();
 
-        void loop_the_program(){
-            while (1){
-                cin >> command;
-                if (command == "-exit"){
-                   break;
-                 }else if (command == "-a"){
-                    add_new_task();
-                 }else if (command == "-p"){
-                    add_priority();
-                 }
-            }
-        }
-
+    public:
         void add_new_task(){
             cin >> task;
             taskVector.push_back(task);
-            /*for (unsigned int i = 0; i < taskVector.size(); ++i){
+            for (unsigned int i = 0; i < taskVector.size(); ++i){                  //debug
             cout << taskVector[i] << endl;
-            }*/
-            }
+           }
+        }
         void add_priority(){
             cin >> priority;
             priorityVector.push_back(priority);
-            //for (unsigned int i = 0; i < priorityVector.size(); ++i){
-            // cout << priorityVector[i] << taskVector[i] << endl;                     //paraszt debug
-            // }
-            }
-};
+            for (unsigned int i = 0; i < priorityVector.size(); ++i){             //debug
+            cout << priorityVector[i] << taskVector[i] << endl;
+           }
+        }
+       /* void list_all_task(){
 
-class welcome{
+        }*/
+};
+class Engine: public Todos{
+    protected:
+        string command = "";
+        Todos todos_ojbect;
     public:
-    void print_welcome_screen(){
-        cout << "\tTodo application" << endl
-        << "============================== " << endl
-        << "Commands:" << endl
-        << "-a     Adds a new task" << endl
-        << "-wr    Write current todos to file" << endl
-        << "-rd    Read todos from a file" << endl
-        << "-l     Lists all the tasks" << endl
-        << "-e     Empty the list" << endl
-        << "-rm    Removes a task" << endl
-        << "-c     Completes a task" << endl
-        << "-p     Add priority to a task" << endl
-        << "-lp    Lists all the tasks by priority " << endl
-        << "-exit  Exit from the program" << endl << endl;}
+        void program_engine(){
+            while (1){
+                cin >> command;
+                if (command == "-exit"){
+                    cout << command;
+                    break;
+                }else if (command == "-a"){
+                todos_ojbect.add_new_task();
+                //cout << "test";                                               //debug
+                }else if (command == "-p"){
+                todos_ojbect.add_priority();                                    //debug
+                //cout << "test2";
+                }
+            }
+        }
 };
 
+class Welcome{
+    public:
+        void print_welcome_screen(){
+            cout << "\tTodo application" << endl
+            << "============================== " << endl
+            << "Commands:" << endl
+            << "-a     Adds a new task" << endl
+            << "-wr    Write current todos to file" << endl
+            << "-rd    Read todos from a file" << endl
+            << "-l     Lists all the tasks" << endl
+            << "-e     Empty the list" << endl
+            << "-rm    Removes a task" << endl
+            << "-c     Completes a task" << endl
+            << "-p     Add priority to a task" << endl
+            << "-lp    Lists all the tasks by priority " << endl
+            << "-exit  Exit from the program" << endl << endl;}
+};
 
 int main() {
-    welcome welcome_object;
+    Welcome welcome_object;
     welcome_object.print_welcome_screen();
 
-    todos todos_oject;
-    todos_oject.loop_the_program();
-    todos_oject.add_new_task();
-    todos_oject.add_priority();
+
+    Engine engine_object;
+
+    engine_object.program_engine();
+    //engine_object.add_new_task();
+    //engine_object.add_priority();
+    //todos_ojbect.program_engine();
+    //todos_ojbect.add_new_task();
+    //todos_ojbect.add_priority();
     return 0;
 }
 

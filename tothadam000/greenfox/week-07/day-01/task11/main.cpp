1d8aa310b8788b33814afbb868956bc634f516fe
Toth Adam <tothadam000@gmail.com>
2017-11-20 17:14:56 +0100

    task11_add

---
+++ tothadam000/greenfox/week-07/day-01/task11/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main() {
+	srand(time(NULL)); // This initializes the random generator.
+	int rnd = 0;
+	for (int i=0; i<20; i++) {
+		rnd = rand() % 5; // generate a random number in the range [0, 4]
+		try {
+			switch (rnd) {
+				case 0:
+					throw 3.2f;
+					break;
+				case 1:
+					throw double(43.56);
+					break;
+				case 3:
+					throw "Abrakadabra";
+					break;
+				case 4:
+					throw false;
+					break;
+				default:
+					throw 12;
+					break;
+			}
+		}
+// Write catch blocks for each possibly thrown exception.
+// In each catch block write out what the exception's type was. And what the exceptions value was.
+
+        catch (float error) {
+            cout << "error massage: " << error << endl;
+        }catch (double error) {
+            cout << "error massage: " << error << endl;
+        }catch (const char *error) {
+            cout << "error massage: " << error << endl;
+        }catch (bool error) {
+            cout << "error massage: " << "bool: " <<error << endl;
+        }catch (int error) {
+            cout << "error massage: " << error << endl;
+        }
+	}
+	return 0;
+}

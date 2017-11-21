9e4490b05f03c16b64365df2b9be947097a40550
Toth Adam <tothadam000@gmail.com>
2017-11-21 13:34:02 +0100

    task01_add

---
+++ tothadam000/greenfox/week-07/day-02/task04/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Create a Class named Integer. It should have one int member variable.
+// Create add() methods to it. They should take Integer& parameters.
+// Make variations of add() method 1, 2, 3, 4 and 5 arguments respectively.
+// All methods should add the value of the parameters to the member variable of this.
+//
+// Try them out. Write code that demonstrates how they work.
+
+class Integer{
+    private:
+        int it;
+    public:
+        void set_it(int it){
+            this->it = it;
+        }
+
+};
+
+int main() {
+
+
+	return 0;
+}

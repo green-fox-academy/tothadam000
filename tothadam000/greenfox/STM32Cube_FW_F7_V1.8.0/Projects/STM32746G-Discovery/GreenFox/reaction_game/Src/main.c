88a8ef6c51ee86cb118346ded95bfd5d98cd7e7c
unknown <tothadam000@gmail.com>
2017-11-30 12:41:29 +0100

    reaction_game_add

--- tothadam000/greenfox/STM32Cube_FW_F7_V1.8.0/Projects/STM32746G-Discovery/GreenFox/reaction_game/Src/main.c
+++ tothadam000/greenfox/STM32Cube_FW_F7_V1.8.0/Projects/STM32746G-Discovery/GreenFox/reaction_game/Src/main.c
@@ -1,10 +1,10 @@
  /**
   ******************************************************************************
-  * @file    Templates/Src/main.c
+  * @file    Templates/Src/main.c 
   * @author  MCD Application Team
   * @version V1.0.3
-  * @date    22-April-2016
-  * @brief   STM32F7xx HAL API Template project
+  * @date    22-April-2016 
+  * @brief   STM32F7xx HAL API Template project 
   ******************************************************************************
   * @attention
   *
@@ -37,6 +37,8 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include <string.h>
+
 
 /** @addtogroup STM32F7xx_HAL_Examples
   * @{
@@ -44,13 +46,23 @@
 
 /** @addtogroup Templates
   * @{
-  */
-
+  */ 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef uart_handle;
+
 /* Private function prototypes -----------------------------------------------*/
+
+#ifdef __GNUC__
+/* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
 static void SystemClock_Config(void);
 static void Error_Handler(void);
 static void MPU_Config(void);
@@ -63,28 +75,19 @@ static void CPU_CACHE_Enable(void);
   * @param  None
   * @retval None
   */
-
-
-
 int main(void)
 {
-
-  /* This project template calls firstly two functions in order to configure MPU feature
+  /* This project template calls firstly two functions in order to configure MPU feature 
      and to enable the CPU Cache, respectively MPU_Config() and CPU_CACHE_Enable().
-     These functions are provided as template implementation that User may integrate
-     in his application, to enhance the performance in case of use of AXI interface
-     with several masters. */
-
+     These functions are provided as template implementation that User may integrate 
+     in his application, to enhance the performance in case of use of AXI interface 
+     with several masters. */ 
+  
   /* Configure the MPU attributes as Write Through */
   MPU_Config();
 
   /* Enable the CPU Cache */
   CPU_CACHE_Enable();
-
-  HAL_Init();
-
-    /* Configure the System clock to have a frequency of 216 MHz */
-    SystemClock_Config();
 
   /* STM32F7xx HAL library initialization:
        - Configure the Flash ART accelerator on ITCM interface
@@ -92,115 +95,64 @@ int main(void)
        - Set NVIC Group Priority to 4
        - Low Level Initialization
      */
-	__HAL_RCC_GPIOA_CLK_ENABLE();    // we need to enable the GPIOA port's clock first
+  HAL_Init();
 
-	GPIO_InitTypeDef tda;            // create a config structure
-	tda.Pin = GPIO_PIN_0;            // this is about PIN 0
-	tda.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
-	tda.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
-	tda.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
-
-	HAL_GPIO_Init(GPIOA, &tda);      // initialize the pin on GPIOA port with HAL;
+  /* Configure the System clock to have a frequency of 216 MHz */
+  SystemClock_Config();
 
 
-	__HAL_RCC_GPIOF_CLK_ENABLE();
+  /* Add your application code here
+     */
+  BSP_LED_Init(LED_GREEN);
+  BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
 
-	GPIO_InitTypeDef tda1;            // create a config structure
-	tda1.Pin = GPIO_PIN_10;            // this is about PIN 1
-	tda1.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
-	tda1.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
-	tda1.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
+  uart_handle.Init.BaudRate   = 115200;
+  uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
+  uart_handle.Init.StopBits   = UART_STOPBITS_1;
+  uart_handle.Init.Parity     = UART_PARITY_NONE;
+  uart_handle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
+  uart_handle.Init.Mode       = UART_MODE_TX_RX;
 
-	HAL_GPIO_Init(GPIOF, &tda1);      // initialize the pin on GPIOF port with HAL;
+  BSP_COM_Init(COM1, &uart_handle);
 
-	GPIO_InitTypeDef tda2;            // create a config structure
-	tda2.Pin = GPIO_PIN_9;            // this is about PIN 9
-	tda2.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
-	tda2.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
-	tda2.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
+  /* Output a message using printf function */
+  printf("\n------------------WELCOME------------------\r\n");
+  printf("**********in STATIC reaction game**********\r\n\n");
 
-	HAL_GPIO_Init(GPIOF, &tda2);      // initialize the pin on GPIOA port with HAL;
+  RNG_HandleTypeDef rnd;
+   uint32_t rnd_num;
+   rnd.Instance     = RNG;
+   HAL_RNG_Init(&rnd);
+   HAL_RNG_GenerateRandomNumber(&rnd, &rnd_num);
 
-	GPIO_InitTypeDef tda3;            // create a config structure
-	tda3.Pin = GPIO_PIN_8;            // this is about PIN 8
-	tda3.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
-	tda3.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
-	tda3.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
+   rnd_num = rnd_num % 10 + 1,
 
-	HAL_GPIO_Init(GPIOF, &tda3);      // initialize the pin on GPIOF port with HAL;
-
-	GPIO_InitTypeDef tda4;            // create a config structure
-	tda4.Pin = GPIO_PIN_7;            // this is about PIN 87
-	tda4.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
-	tda4.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
-	tda4.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
-
-	HAL_GPIO_Init(GPIOF, &tda4);      // initialize the pin on GPIOF port with HAL;
-
-	GPIO_InitTypeDef button;
-	button.Pin = GPIO_PIN_6;
-	button.Mode = GPIO_MODE_INPUT;
-	button.Pull = GPIO_PULLUP;
-	button.Speed = GPIO_SPEED_HIGH;
-
-	HAL_GPIO_Init(GPIOF, &button);
-	
-	RNG_HandleTypdef
-	
-	int button_counter = 0;
-
-	while(1)
-	{
-		  if (HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_6) == 0)
-		   {
-			  button_counter = 1;
-
-			while (button_counter = 1){
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_SET);   // setting the pin to 1
-
-			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);   // setting the pin to 1
-
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_SET);   // setting the pin to 1
-
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET);   // setting the pin to 1
-
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET);   // setting the pin to 1
-
-			HAL_Delay(100);
-
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_RESET);   // setting the pin to 1
-
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET);   // setting the pin to 1
-
-			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);   // setting the pin to 1
-
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET);   // setting the pin to 1
-
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET);   // setting the pin to 1
-
-			HAL_Delay(100);
-
-			if (HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_6) == 0){
-
-				HAL_Delay(200);
-				if (HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_6) == 0){
-					button_counter = 0;
-					break;
-				}
-				break;
+   printf("%i\n", rnd_num );
 
 
-			}
-		   }
-		  }
-   }
 
+  while (1)
+  {
 
+  }
+}
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
+  HAL_UART_Transmit(&uart_handle, (uint8_t *)&ch, 1, 0xFFFF);
+
+  return ch;
 }
 
 /**
   * @brief  System Clock Configuration
-  *         The system Clock is configured as follow :
+  *         The system Clock is configured as follow : 
   *            System Clock source            = PLL (HSE)
   *            SYSCLK(Hz)                     = 216000000
   *            HCLK(Hz)                       = 216000000
@@ -218,7 +170,6 @@ int main(void)
   * @param  None
   * @retval None
   */
-
 static void SystemClock_Config(void)
 {
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
@@ -231,7 +182,7 @@ static void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 432;
+  RCC_OscInitStruct.PLL.PLLN = 432;  
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 9;
   if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
@@ -244,14 +195,14 @@ static void SystemClock_Config(void)
   {
     Error_Handler();
   }
-
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
      clocks dividers */
   RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
   if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
   {
     Error_Handler();
@@ -281,7 +232,7 @@ static void Error_Handler(void)
 static void MPU_Config(void)
 {
   MPU_Region_InitTypeDef MPU_InitStruct;
-
+  
   /* Disable the MPU */
   HAL_MPU_Disable();
 
@@ -328,7 +279,7 @@ static void CPU_CACHE_Enable(void)
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{
+{ 
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
@@ -341,10 +292,10 @@ void assert_failed(uint8_t* file, uint32_t line)
 
 /**
   * @}
-  */
+  */ 
 
 /**
   * @}
-  */
+  */ 
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/

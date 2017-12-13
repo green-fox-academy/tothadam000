ab7f8fa022c2aa4459cc62dc5a058f4771883f6d
unknown <tothadam000@gmail.com>
2017-12-13 14:29:08 +0100

    03_add

--- tothadam000/greenfox/week-09-PractieDay/03/main.cpp
+++ tothadam000/greenfox/week-09-PractieDay/03/main.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 class Car{
     private:
-        string type= "Mustang";
-        int km = 5871;
+        string type;
+        int km;
 
     public:
         Car (string type, int km){
             this -> type = type;
             this -> km = km;
         }
-        void run(){
+        void run(int km){
+            this -> km += km;
+            cout << type << ": " <<  this -> km << endl;
 
         }
 };
@@ -24,6 +26,12 @@ int main() {
     // It should have a "km" property that stores how many kilometers it have run
     // The km and the type property should be a parmeter in the constructor
     // It should have a method called "run" that takes a number and increments the "km" property by it
+
+    Car c("Mustang", 1000);
+    c.run(469);
+    // vagy igy:
+    Car car = Car("Ocskavas", 50000);
+    car.run(500);
 
   return 0;
 }

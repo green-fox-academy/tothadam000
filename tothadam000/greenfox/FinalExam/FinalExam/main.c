250a62af977777de09cbea2feb572082028b5aff
Adam <tothadam000@gmail.com>
2018-02-02 10:10:45 +0100

    final_exam

---
+++ tothadam000/greenfox/FinalExam/FinalExam/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Create a function that takes an array of floats and returns with the average of the smallest and the highest value
+
+float avarage();
+
+int main()
+{
+    printf("%f", avarage());
+
+    return 0;
+}
+
+float avarage(){
+    float nummbers[] = {100.798, 2.6, 3.4, 7.0, 50.9};
+    float highest;
+    float smallest;
+
+    highest = nummbers[0];
+
+    int i = 0;
+
+    for (i = 1; i < 5; i++)
+    {
+        if (highest < nummbers[i])
+            highest = nummbers[i];
+    }
+    //printf("\n highest element present in the given array is : %f", highest);
+
+    smallest = nummbers[0];
+
+    int a = 0;
+
+    for (a = 1; a < 5; a++)
+    {
+        if (smallest > nummbers[a])
+            smallest = nummbers[a];
+    }
+    //printf("\n smallest element present in the given array is : %f", smallest);
+
+
+    return (highest + smallest)/2;
+}
+
+
